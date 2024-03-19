7bdb0c979a67a889523145330ba06de120dc9b31
trishaDas13 <trisha.das1308@gmail.com>
2024-03-19 11:59:07 +0530

    get and post api added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import "./style.scss";
 import { Button, Modal } from "antd";
 import Card from "./Card";
+import axios from "axios";
 
 const initialState = {
   name: "",
@@ -42,25 +43,58 @@ const reducer = (state, action) => {
 };
 const Main = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [employeeList, setEmployeeList] = useState([]);
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  //todo: create button from modal
-  const handleCreateEmployee = () => {
-    if (
-      state.name === "" ||
-      state.email === "" ||
-      state.department === "" ||
-      state.designation === "" ||
-      state.dateOfJoining === "" ||
-      state.salary === ""
-    ) {
-      alert("Please fill in all fields");
-    } else {
-      dispatch({ type: "addEmployee" });
-      setModalOpen(false);
+   //todo: fetch data from api
+  const getEmployee = async () => {
+    try {
+      let res = await axios.get(
+        "https://addemployee.onrender.com/dashboard/employee/get-data"
+      );
+      setEmployeeList(res.data.result);
+    } catch (error) {
+      console.log("facing error at the of fwtching data", error);
     }
   };
-  
+
+  //todo: post data to api
+  const addEmployeeToServer = async () => {
+    let employeeInfo = {
+      name: state.name,
+      email: state.email,
+      department: state.department,
+      designation: state.designation,
+      dateOfJoining: state.dateOfJoining,
+      salary: state.salary,
+    };
+    try {
+      let res = await axios.post("https://addemployee.onrender.com/dashboard/employee/post-data", employeeInfo);
+      if (
+        employeeInfo.name === "" ||
+        employeeInfo.email === "" ||
+        employeeInfo.department === "" ||
+        employeeInfo.designation === "" ||
+        employeeInfo.dateOfJoining === "" ||
+        employeeInfo.salary === ""
+      ) {
+        alert("Please fill in all fields");
+        return;
+      } else {
+        dispatch({ type: "addEmployee" });
+        getEmployee();
+        setModalOpen(false);
+      }
+      
+    } catch (err) {
+      console.log("Error while adding Employee ", err);
+    }
+  };
+
+  useEffect(() => {
+    getEmployee();
+  }, []);
+
   return (
     <main className="main">
       <button className="add" onClick={() => setModalOpen(true)}>
@@ -71,13 +105,7 @@ const Main = () => {
         centered
         open={modalOpen}
         onCancel={() => setModalOpen(false)}
-        footer={
-          <Button
-            onClick={handleCreateEmployee}
-          >
-            Create
-          </Button>
-        }
+        footer={<Button onClick={addEmployeeToServer}>Create</Button>}
       >
         <form action="">
           <input
@@ -163,7 +191,7 @@ const Main = () => {
       </Modal>
 
       <div className="employeeCards">
-        {state.employees.map((employee, index) => (
+        {employeeList.map((employee, index) => (
           <Card key={index} employee={employee} />
         ))}
       </div>
